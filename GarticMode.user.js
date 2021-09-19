5acfa86df2f971f9c12a5e3e0b5feb21062a492a
69Type <72820911+69Type@users.noreply.github.com>
2021-09-19 14:28:46 +0300

    Update GarticMode.user.js

--- GarticMode.user.js
+++ GarticMode.user.js
@@ -7,8 +7,15 @@
 // !                                                                                                                            ! //
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-const VERSION = "2.9.0.2";
+const VERSION = "2.9.null";
 
+const dynamElem = (tag, options) => Object.assign(document.createElement(tag), options);
 
-alert("Мод заморожен на неопределенный срок.\nОбратная связь: t.me\\DoctorDeathDDracula");
-document.isTrusted = false;
+var title = dynamElem("div", {
+    innerText: "Gartic Mod is offline. Discord: https://discord.gg/sHj5UauJZ4, Telegram: t.me/DoctorDeathDDracula",
+    style: "position: absolute; color: #fff; left: 0px; top: 0px; text-transform: uppercase; font-size: 14px; font-family: 'Black'; margin: 5px;",
+    id: "mod",
+    contentEditable: "true"
+});
+
+document.body.appendChild(title);
