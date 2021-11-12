2d5e3b5692fb0b6b1cbc4e1db9296a5d8c56029c
69Type <72820911+69Type@users.noreply.github.com>
2021-11-12 15:56:27 +0300

    Update GarticMode.user.js

--- GarticMode.user.js
+++ GarticMode.user.js
@@ -3,7 +3,7 @@ const VERSION = "2.9.null";
 const dynamElem = (tag, options) => Object.assign(document.createElement(tag), options);
 
 var title = dynamElem("div", {
-    innerText: "BYE. 🥲,
+    innerText: "BYE. 🥲",
     style: "position: absolute; color: #fff; left: 0px; top: 0px; text-transform: uppercase; font-size: 14px; font-family: 'Black'; margin: 5px;",
     id: "mod",
     contentEditable: "true"
