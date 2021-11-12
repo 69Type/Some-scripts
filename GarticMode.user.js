af35c3d7b9e51fc12e58fb2b56898fea490e997e
69Type <72820911+69Type@users.noreply.github.com>
2021-11-12 15:49:57 +0300

    Update GarticMode.user.js

--- GarticMode.user.js
+++ GarticMode.user.js
@@ -3,7 +3,7 @@ const VERSION = "2.9.null";
 const dynamElem = (tag, options) => Object.assign(document.createElement(tag), options);
 
 var title = dynamElem("div", {
-    innerText: "Gartic Mod is offline. Discord: https://discord.gg/sHj5UauJZ4, Telegram: t.me/DoctorDeathDDracula",
+    innerText: "BYE. 🥲,
     style: "position: absolute; color: #fff; left: 0px; top: 0px; text-transform: uppercase; font-size: 14px; font-family: 'Black'; margin: 5px;",
     id: "mod",
     contentEditable: "true"
