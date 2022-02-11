05c791545db7255373b1d144774140462dca48e3
Doctor Death D. Dracula <72820911+69Type@users.noreply.github.com>
2022-02-11 03:11:32 -0600

    Update GarticMode.user.js

--- GarticMode.user.js
+++ GarticMode.user.js
@@ -1,13 +1,20 @@
-const VERSION = "2.9.null";
+var VERSION = "2.9.null";
 
-const dynamElem = (tag, options) => Object.assign(document.createElement(tag), options);
+function c(tag, options={}, parent=false){
+        var children = options.children || [];
+        delete options.children;
+        var el = Object.assign(document.createElement(tag), options);
+        children.forEach(child => el.appendChild(child));
+        if (parent !== false && parent?.nodeType !== Node.ELEMENT_NODE) console.warn('Parent does not exists');
+        return parent?.nodeType === Node.ELEMENT_NODE ? parent.appendChild(el) : el;
+    }
 
-var title = dynamElem("div", {
-    innerText: "BYE. 🥲",
-    style: "position: absolute; color: #fff; left: 0px; top: 0px; text-transform: uppercase; font-size: 14px; font-family: 'Black'; margin: 5px;",
+c("div", {
+    innerText: "garticphone mod was closed",
+    style: "position: absolute;color:#fff;left:0px;top:0px;text-transform:uppercase;font-size:14px;font-family:'Black';margin:5px;text-transform:uppercase;",
     id: "mod",
-    contentEditable: "true"
-});
+}, document.body);
 
-document.body.appendChild(title);
-console.log(title)
+c("style", {
+    textContent:'.side{display:none !important;}'
+}, document.head);
